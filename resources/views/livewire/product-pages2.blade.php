43dea03e46ae6fa903826ab0b936c1b64e6b76f4
eva nako <nakoeva2000@gmail.com>
2022-11-15 20:14:24 +0100

    added new card element and fixed some details in product page

--- resources/views/livewire/product-pages2.blade.php
+++ resources/views/livewire/product-pages2.blade.php
@@ -1,16 +1,18 @@
-<button class="max-w-xs mx-2 mb-2 shadow-md
-       hover:shadow-lg
-       outline-none
-       focus:outline-none">
+<button class="max-w-xs mx-2 mb-2 hover:shadow-lg">
   <div class="bg-gray-100">
-    <img src="img/imag.png" class="lg:ml-10" />
+    <img src="img/imag.png"/>
   </div>
   <div class="place-content-center">
-    <h5 class="lg:text-base lg:mt-3.5 text-sm font-semibold lg:mb-3 mb-1" style="color:#151875">Vel elit euismod</h5>
-    <img src="img/circle.png" alt="" class="px-32 mb-2">
-    <div class="grid grid-cols-2 lg:mb-10 lg:px-20 ">
-      <h3 class="" style="color:#151875">$26.00 </h3>
-      <h3 class="text-pink-500">$42.00</h3>
+    <h5 class="lg:text-base lg:mt-3.5 text-sm font-semibold lg:mb-2 mb-1" style="color:#151875">Vel elit euismod</h5>
+    <div class="flex place-content-center lg:flex lg:space-x-1 lg:mb-2 lg:place-content-center">
+    <div class="w-2.5 h-2.5 bg-yellow-400 rounded-full"></div>
+    <div class="rounded-full bg-red w-2.5 h-2.5 bg-red-300"> </div>
+    <div class="rounded-full bg-red w-2.5 h-2.5 bg-blue-600"> </div>
+    
+    </div>
+    <div class="grid grid-cols-2 lg:mb-10 lg:flex lg:space-x-3 lg:place-content-center">
+      <h3 class="" style="color:#151875; font-family:Josefin Sans">$26.00 </h3>
+      <h3 class="text-pink-500 line-through" style="font-family: Josefin Sans;">$42.00</h3>
     </div>
   </div>
 </button>
